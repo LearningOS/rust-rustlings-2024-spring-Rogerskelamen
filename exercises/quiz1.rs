ddb500585e691e352f716e184063a2351a62e66d
Rogerskelamen <rogerkelamen@gmail.com>
2024-05-12 20:13:23 +0800

    complete quiz to vec

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n: isize) -> isize {
+    if n > 40 {
+        n
+    } else {
+        n * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
