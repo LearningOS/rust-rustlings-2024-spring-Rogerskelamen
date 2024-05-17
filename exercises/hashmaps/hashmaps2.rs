4922e44df770533c866a677555fae2a781fcada2
Rogerskelamen <rogerskelamen@gmail.com>
2024-05-16 22:39:11 -0700

    complete hashmap quiz

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -40,6 +38,16 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        let mut flag = true;
+        for good in basket.keys() {
+            if fruit == *good {
+                flag = false;
+                break;
+            }
+        }
+        if flag {
+            basket.insert(fruit, 1);
+        }
     }
 }
 
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,10 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -39,6 +38,12 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let scores_1 = scores.entry(team_1_name).or_insert(Team {goals_scored: 0, goals_conceded: 0});
+        scores_1.goals_scored += team_1_score;
+        scores_1.goals_conceded += team_2_score;
+        let scores_2 = scores.entry(team_2_name).or_insert(Team {goals_scored: 0, goals_conceded: 0});
+        scores_2.goals_scored += team_2_score;
+        scores_2.goals_conceded += team_1_score;
     }
     scores
 }
@@ -66,6 +71,7 @@ mod tests {
             keys,
             vec!["England", "France", "Germany", "Italy", "Poland", "Spain"]
         );
+        println!("{:#?}", scores);
     }
 
     #[test]
